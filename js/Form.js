6b3bf398c9cc9c7eadfe98a376f2abcf0e63218b
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2020-04-11 16:34:17 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,29 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+  }
+  hide(){
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth-100,20);
+    var name = this.input.value();
+    this.button.mousePressed(()=>{
+      this.hide();
+      this.greeting.html("Hello " + name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    })
+  }
+}
